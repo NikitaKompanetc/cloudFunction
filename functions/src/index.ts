75b11125f3419985ecca87389bd94331d2ac515d
NikitaKompanetc <kni2012@ukr.net>
2021-06-03 13:21:46 +0300

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,8 @@ import * as admin from "firebase-admin";
 admin.initializeApp();
 
 export const scheduledFunctionPlainEnglish = functions.pubsub
-  .schedule("0 15 * * *")
+  .schedule("0 1 * * *")
+  .timeZone("Australia/Brisbane")
   .onRun(async () => {
     return (await admin.firestore().collection("items").get()).docs.forEach(
       (doc) => {
